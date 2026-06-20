d51fff5e666473ca11141418f1206f36bcf5795d
mao4864 <2318440326@qq.com>
2026-06-21 06:21:12 +0800

    Adjust Hero component height to 90vh

--- themes/hexo/components/Hero.js
+++ themes/hexo/components/Hero.js
@@ -59,7 +59,7 @@ const Hero = props => {
     <header
       id='header'
       style={{ zIndex: 1 }}
-      className='w-full h-screen relative bg-black'>
+      className='w-full h-[90vh] relative bg-black'
       <div className='text-white absolute bottom-0 flex flex-col h-full items-center justify-center w-full '>
         {/* 站点标题 */}
         <div className='font-black text-4xl md:text-5xl shadow-text'>
