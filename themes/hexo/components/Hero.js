731df712f3e6677a7bd9bd19883abe2edcb89138
mao4864 <2318440326@qq.com>
2026-06-21 18:08:26 +0800

    Adjust padding in Hero component header

--- themes/hexo/components/Hero.js
+++ themes/hexo/components/Hero.js
@@ -60,7 +60,7 @@ const Hero = props => {
       id='header'
       style={{ zIndex: 1 }}
       className='w-full h-screen relative bg-black'>
-      <div className='text-white absolute top-0 flex flex-col h-full items-center justify-start w-full pt-13'>
+      <div className='text-white absolute top-0 flex flex-col h-full items-center justify-start w-full pt-16'>
         {/* 站点标题 */}
         <div className='font-black text-5xl md:text-6xl shadow-text'>
           {siteInfo?.title || siteConfig('TITLE')}
