007217fc1987bebe75988e4d007c9b589e91e113
mao4864 <2318440326@qq.com>
2026-06-21 16:05:20 +0800

    Update Hero component layout and styles

--- themes/hexo/components/Hero.js
+++ themes/hexo/components/Hero.js
@@ -62,7 +62,7 @@ const Hero = props => {
       className='w-full h-screen relative bg-black'>
       <div className='text-white absolute bottom-0 flex flex-col h-full items-center justify-center w-full '>
         {/* 站点标题 */}
-        <div className='font-black text-4xl md:text-5xl shadow-text'>
+        <div className='text-white absolute bottom-0 flex flex-col h-full items-center justify-start w-full pt-20'>
           {siteInfo?.title || siteConfig('TITLE')}
         </div>
         {/* 站点欢迎语 */}
